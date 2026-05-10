c31ec4f52beba011532efdc7993a69e206af7a44
VV <vishun.orv@gmail.com>
2026-05-10 13:41:14 +0530

    Fix: Switch PDF worker to unpkg CDN for reliability

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -36,7 +36,7 @@ export function PdfViewer(props: PdfViewerProps) {
       Document = rp.Document;
       Page = rp.Page;
       pdfjs = rp.pdfjs;
-      pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+      pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
       setIsReady(true);
     };
     loadPdf();
